c82fb72fc07bf3cee5ecac506510a1502db8f00f
vinit kumar <vkr82663@gmail.com>
2026-06-15 22:45:33 +0530

    initial commit

---
+++ ERP_Backend/Backend_Api/Backend_Api/Model/Doctor.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend_Api.Model
+{
+    public class Doctor
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}
